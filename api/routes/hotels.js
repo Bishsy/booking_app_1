ddd1a85ed7f5091cbacd7b86f954c94996afc1db
Bishy <sonarbishal1@gmail.com>
2023-08-10 18:54:16 +0530

    first commit

---
+++ api/routes/hotels.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+const router = express.Router();
+
+//create
+router.post("/", aysnc(req,res)=> {
+try {
+    
+} catch (error) {
+    res.status(500)
+}
+})
+//update
+//delete
+//get
+//get all
+
+
+export default  router
